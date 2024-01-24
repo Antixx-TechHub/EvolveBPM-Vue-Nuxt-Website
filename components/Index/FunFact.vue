11cfcd0d8e354203acde1948fa8819ac5694be31
AntixxTechHub <info@antixxtechhub.com>
2024-01-24 20:46:54 +0530

    funfact done

--- components/Index/FunFact.vue
+++ components/Index/FunFact.vue
@@ -2,32 +2,33 @@
   <section class="about-sec-app pad-tb pt-60 dark-bg2">
     <div class="container">
       <div class="row">
-        <div class="col-lg-6">
-          <div class="common-heading text-l">
-            <h4 class="mb-30"> At Evolve, we have curated a comprehensive lead generation ecosystem – built on
-              best-in-class practices. Within this ecosystem of industry experts, we provide unique dedicated communities
-              for each client.</h4>
-            
-            <h5>Having a dedicated unit just for your needs allows us to ensure -</h5>
-
-            <p class="mt-10">Alignment with your culture and core values</p>
-            <p class="mt-10">Teams trained to be your extension units with complete visibility and control.</p>
-            <p class="mt-10">Predictable outcomes through shared planning and execution. </p>
-            <p class="mt-10">Transparency – Of how your unit is functioning through access
-              to operational metrics and systems. </p>
-
-            <p class="mt-10"> Accountability – Of our deliveries.</p>
-
-            <p class="mt-10">Authenticity – Of every lead we capture.</p>
-
-            <p class="mt-10">Security – Of your & clients and data. </p>
-
-
+        <div class="col-lg-9">
+          <div class="email-subs">
+            <h5 class="mb-30">At Evolve, we have curated a comprehensive lead generation ecosystem - built<br>
+              on best-in-class practices. Within this ecosystem of industry experts, we<br>
+              provide unique dedicated communities for each client.</h5>
           </div>
-          <!-- <div class="cta-card mt-40">
-            <a href="#" class="btn-outline lnk">Request A Quote<i class="fas fa-chevron-right fa-icon"></i><span
-                class="circle"></span></a>
-          </div> -->
+        </div>
+        <div class="col-lg-3 d-flex align-items-center">
+          <a href="#" class="btn-main bg-btn">Request A Demo <i class="fas fa-chevron-right fa-ani"></i></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="col-md-12 mt30">
+            <div class="niwax-list">
+              <p><strong>Having a dedicated unit just for your needs allows us to ensure -</strong></p>
+              <ul class="key-points">
+                <li>Alignment with your culture and core values.</li>
+                <li>Teams trained to be your extension units with complete visibility and control.</li>
+                <li>Predictable outcomes through shared planning and execution.</li>
+                <li>Transparency – Of how your unit is functioning through access to operational metrics and systems.</li>
+                <li>Accountability – Of our deliveries.</li>
+                <li>Authenticity – Of every lead we capture.</li>
+                <li>Security – Of your & clients and data.</li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div class="col-lg-6">
           <div class="funfact">
