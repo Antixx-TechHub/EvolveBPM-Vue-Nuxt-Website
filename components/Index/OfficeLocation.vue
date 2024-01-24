4a1e97cdf0ad828d64029d70633f5519b4ec0497
AntixxTechHub <info@antixxtechhub.com>
2024-01-24 20:07:47 +0530

    locacation

--- components/Index/OfficeLocation.vue
+++ components/Index/OfficeLocation.vue
@@ -1,69 +1,81 @@
 <template>
-<section class="our-office pad-tb">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-8">
-                <div class="common-heading">
-                    <span>Our Locations</span>
-                    <h2>Our Office</h2>
+    <section class="our-office pad-tb">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="common-heading">
+                        <span>Our Locations</span>
+                        <h2>Our Office</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".2s">
+                    <div class="office-card hoshd">
+                        <div class="landscp">
+                            <img src="~/assets/images/location/newyork.png" alt="location" class="img-fluid" />
+                        </div>
+                        <div class="info-text-div">
+                            <h4>New York</h4>
+                            <p>99 Wall Street #3966 New York,<br>NY 10005.</p>
+                            <ul class="list-unstyled -address-list mt-3">
+                                <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
+                                        corporate@evolvebpm.com</a></li>
+                                <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
+                            </ul>
+                            <div class="office-text">
+                                <a href="#" class="mt10 btn-main btn-small bg-btn3 lnk">View on Map<i
+                                        class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".4s">
+                    <div class="office-card hoshd">
+                        <div class="landscp">
+                            <img src="~/assets/images/location/uk.png" alt="location" class="img-fluid" />
+                        </div>
+                        <div class="info-text-div">
+                            <h4>UK</h4>
+                            <p>12 Constance Street, London, England E16 2DQ, GB.</p>
+                            <ul class="list-unstyled -address-list mt-3">
+                                <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
+                                        corporate@evolvebpm.com</a></li>
+                                <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
+                            </ul>
+                            <div class="office-text">
+                                <a href="#" class="mt10 btn-main btn-small bg-btn3 lnk">View on Map<i
+                                        class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".6s">
+                    <div class="office-card hoshd">
+                        <div class="landscp">
+                            <img src="~/assets/images/location/india.png" alt="location" class="img-fluid" />
+                        </div>
+                        <div class="info-text-div">
+                            <h4>India</h4>
+                            <p>Old Madras Road, 903 Brigade Paramount, Bangalore 560093, IN.</p>
+                            <ul class="list-unstyled -address-list mt-3">
+                                <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
+                                        corporate@evolvebpm.com</a></li>
+                                <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
+                            </ul>
+                            <div class="office-text">
+                                <a href="#" class="mt10 btn-main btn-small bg-btn3 lnk">View on Map<i
+                                        class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="row justify-content-center">
-            <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".2s">
-                <div class="office-card hoshd">
-                    <div class="landscp">
-                        <img src="~/assets/images/location/newyork.png" alt="location" class="img-fluid" />
-                    </div>
-                    <div class="info-text-div">
-                        <h4>New York</h4>
-                        <p>99 Wall Street #3966 New York,<br>NY 10005.</p>
-                        <ul class="list-unstyled -address-list mt-3">
-                            <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
-                                corporate@evolvebpm.com</a></li>
-                            <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".4s">
-                <div class="office-card hoshd">
-                    <div class="landscp">
-                        <img src="~/assets/images/location/uk.png" alt="location" class="img-fluid" />
-                    </div>
-                    <div class="info-text-div">
-                        <h4>UK</h4>
-                        <p>12 Constance Street, London, England E16 2DQ, GB.</p>
-                        <ul class="list-unstyled -address-list mt-3">
-                            <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
-                                corporate@evolvebpm.com</a></li>
-                            <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-sm-6 shape-loc wow fadeIn" data-wow-delay=".6s">
-                <div class="office-card hoshd">
-                    <div class="landscp">
-                        <img src="~/assets/images/location/india.png" alt="location" class="img-fluid" />
-                    </div>
-                    <div class="info-text-div">
-                        <h4>India</h4>
-                        <p>Old Madras Road, 903 Brigade Paramount, Bangalore 560093, IN.</p>
-                        <ul class="list-unstyled -address-list mt-3">
-                            <li><a href="mailto:corporate@evolvebpm.com"><i class="fas fa-envelope"></i>
-                                corporate@evolvebpm.com</a></li>
-                            <li><a href="tel:+10000000000"><i class="fas fa-phone-alt"></i> +91-020-69029999</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
 </template>
 
 <script>
