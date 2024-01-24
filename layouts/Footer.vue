8a4ce7b2e3f564ffd3d7c7a239d98194aae51cae
AntixxTechHub <info@antixxtechhub.com>
2024-01-24 22:58:31 +0530

    footer done

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,163 +1,113 @@
-<!-- components/Footer.vue -->
 <template>
-    <footer class="dark-footer dg-bg--1 pt60 nshape dark-footer-1">
-        <div class="container">
-            <div class="row justify-content-between">
-
-                <!-- Previous code -->
-                <div class="col-lg-4 col-sm-6 ftr-brand-pp">
-                    <!-- ... (Previous code) ... -->
+    <div class="footerdez footerdex dark-footer pt50">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-lg-12 col-sm-12">
+            <div class="row fttlnks">
+              <div class="col-lg-4 col-sm-12">
+                <div class="footerdez-a">
+                  <NuxtLink class="navbar-brand mt30 f-dark-logo" to="/">
+                    <img src="~/assets/images/white-logo.png" alt="Logo" />
+                  </NuxtLink>
+                  <p class="mt10">
+                    Lorem ipsum dolor sit amet, is consectetur adipiscing elit,
+                    sed do ipsum lorem dollar eiusmo. Lorem ipsum dolor sit amet,
+                    is sit consectetur adipiscing elit.
+                  </p>
                 </div>
-
-                <div class="col-lg-3 col-sm-6">
-                    <!-- ... (Previous code) ... -->
+                <h5 class="mt20">Follow Us</h5>
+                <div class="ff-social-icons">
+                  <a href="javascript:void(0)" target="blank"
+                    ><i class="fab fa-facebook"></i
+                  ></a>
+                  <a href="javascript:void(0)" target="blank"
+                    ><i class="fab fa-twitter"></i
+                  ></a>
+                  <a href="javascript:void(0)" target="blank"
+                    ><i class="fab fa-instagram"></i
+                  ></a>
+                  <a href="javascript:void(0)" target="blank"
+                    ><i class="fab fa-linkedin"></i
+                  ></a>
+                  <a href="javascript:void(0)" target="blank"
+                    ><i class="fab fa-youtube"></i
+                  ></a>
                 </div>
-
-                <div class="col-lg-2 col-sm-6">
-                    <!-- ... (Previous code) ... -->
-                </div>
-
-                <div class="col-lg-3 col-sm-6 footer-blog-">
-                    <!-- ... (Previous code) ... -->
-                </div>
-
-                <!-- New code -->
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="single-footer-card">
-                        <NuxtLink to="/" class="logo">
-                            <img src="~/assets/images/white-logo.png" alt="logo">
-                        </NuxtLink>
-                        <p>Lorem ipsum dolor sit amet, is consectetur adipiscing elit, sed do ipsum lorem dollar eiusmo.
-                            Lorem ipsum dolor sit amet, is sit consectetur adipiscing elit.</p>
-                        <ul class="social-links">
-                            <!-- ... (New code) ... -->
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="footer-address-list link-hover">
-                        <h5 class="mb30 mt30">Company</h5>
-                        <ul class="footer-address-list link-hover">
-                            <li>
-                                <NuxtLink to="/about-us"><i class="fas fa-angle-double-right"></i> About Us</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/leadership"><i class="fas fa-angle-double-right"></i> Leadership</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/our-methodology"><i class="fas fa-angle-double-right"></i> Our Methodology
-                                    (DDIA)</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/careers"><i class="fas fa-angle-double-right"></i> Careers</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/csr-activities"><i class="fas fa-angle-double-right"></i> CSR Activities
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/contact-us"><i class="fas fa-angle-double-right"></i> Contact us</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/request-demo"><i class="fas fa-angle-double-right"></i> Request Demo
-                                </NuxtLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-6 col-sm-6">
-                    <div class="single-footer-card ps-5">
-                        <h5 class="mb30 mt30">Services</h5>
-                        <ul class="footer-address-list link-hover">
-                            <li>
-                                <NuxtLink to="/irev"><i class="fas fa-angle-double-right"></i> iRev</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/idata"><i class="fas fa-angle-double-right"></i> iData</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/imedia"><i class="fas fa-angle-double-right"></i> iMedia</NuxtLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-6 col-sm-6">
-                    <div class="single-footer-card ps-5">
-                        <h5 class="mb30 mt30">Products</h5>
-                        <ul class="footer-address-list link-hover">
-                            <li>
-                                <NuxtLink to="/innovator"><i class="fas fa-angle-double-right"></i> Innovator</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/predictor"><i class="fas fa-angle-double-right"></i> Predictor</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/intello"><i class="fas fa-angle-double-right"></i> Intello</NuxtLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-2 col-md-6 col-sm-6">
-                    <div class="single-footer-card ps-5">
-                        <h5 class="mb30 mt30">Insights</h5>
-                        <ul class="footer-address-list link-hover">
-                            <li>
-                                <NuxtLink to="/blogs"><i class="fas fa-angle-double-right"></i> Blogs</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/pillar-pages"><i class="fas fa-angle-double-right"></i> Pillar pages
-                                </NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/whitepapers"><i class="fas fa-angle-double-right"></i> Whitepapers</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/ebooks"><i class="fas fa-angle-double-right"></i> Ebooks</NuxtLink>
-                            </li>
-                            <li>
-                                <NuxtLink to="/case-studies"><i class="fas fa-angle-double-right"></i> Case studies
-                                </NuxtLink>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
+              </div>
+              <div class="col-lg-2 col-sm-6 pt40">
+                <h5>COMPANY</h5>
+                <ul class="footer-address-list link-hover">
+                  <li><NuxtLink to="/about">About Us</NuxtLink></li>
+                  <li><NuxtLink to="/leadership">Leadership</NuxtLink></li>
+                  <li>
+                    <NuxtLink to="/methodology">Our Methodology (DDIA)</NuxtLink>
+                  </li>
+                  <li><NuxtLink to="/careers">Careers</NuxtLink></li>
+                  <li><NuxtLink to="/csr">CSR Activities</NuxtLink></li>
+                  <li><NuxtLink to="/contact">Contact us</NuxtLink></li>
+                  <li><NuxtLink to="/request-demo">Request Demo</NuxtLink></li>
+                </ul>
+              </div>
+              <div class="col-lg-2 col-sm-6 pt40">
+                <h5>SERVICES</h5>
+                <ul class="footer-address-list link-hover">
+                  <li><NuxtLink to="/services/irev">iRev</NuxtLink></li>
+                  <li><NuxtLink to="/services/idata">iData</NuxtLink></li>
+                  <li><NuxtLink to="/services/imedia">iMedia</NuxtLink></li>
+                </ul>
+              </div>
+              <div class="col-lg-2 col-sm-6 pt40">
+                <h5>PRODUCTS</h5>
+                <ul class="footer-address-list link-hover">
+                  <li><NuxtLink to="/products/innovator">Innovator</NuxtLink></li>
+                  <li><NuxtLink to="/products/predictor">Predictor</NuxtLink></li>
+                  <li><NuxtLink to="/products/intello">Intello</NuxtLink></li>
+                </ul>
+              </div>
+              <div class="col-lg-2 col-sm-6 pt40">
+                <h5>INSIGHTS</h5>
+                <ul class="footer-address-list link-hover">
+                  <li><NuxtLink to="/blogs">Blogs</NuxtLink></li>
+                  <li><NuxtLink to="/pillar-pages">Pillar pages</NuxtLink></li>
+                  <li><NuxtLink to="/whitepapers">Whitepapers</NuxtLink></li>
+                  <li><NuxtLink to="/ebooks">Ebooks</NuxtLink></li>
+                  <li><NuxtLink to="/case-studies">Case studies</NuxtLink></li>
+                </ul>
+              </div>
             </div>
-
-            <div class="row end-footer-">
-                <div class="col-lg-6">
-                    <div class="footer-copyrights-">
-                        <p>&copy; 2024 All rights reserved. <a href="https://www.evolvebpm.com/"
-                                target="_blank">Evolve BPM</a></p>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <ul class="footer-link-- link-hover">
-                        <li>
-                            <NuxtLink to="/privacy-policy">Privacy</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/gdpr">GDPR</NuxtLink>
-                        </li>
-                        <li>
-                            <NuxtLink to="/casl">CASL</NuxtLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
+          </div>
         </div>
-    </footer>
-</template>
+      </div>
   
-<script>
-export default {
-    // Component logic goes here
-}
-</script>
+      <div class="dark-footer dg-bg--2 nshape">
+        <div class="container">
+          <div class="row end-footer-">
+            <div class="col-lg-6">
+              <div class="footer-copyrights-">
+                <p>
+                  &copy; 2024 All rights reserved.
+                  <a href="https://www.evolvebpm.com/" target="_blank"
+                    >Evolve BPM</a
+                  >
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <ul class="footer-link-- link-hover">
+                <li><NuxtLink to="/privacy-policy">Privacy</NuxtLink></li>
+                <li><NuxtLink to="/gdpr">GDPR</NuxtLink></li>
+                <li><NuxtLink to="/casl">CASL</NuxtLink></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
   
-<style scoped>/* Component-specific styles go here */</style>
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
