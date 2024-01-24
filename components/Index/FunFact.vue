ced45de7c0728214e64812744a208447b42e0d4e
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-01-24 17:35:04 +0530

    changes updated stats fundact

--- components/Index/FunFact.vue
+++ components/Index/FunFact.vue
@@ -1,75 +1,90 @@
 <template>
-<section class="about-sec-app pad-tb pt-60 dark-bg2">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="common-heading text-l">
-          <h2 class="mb-30"><span class="text-second text-bold">Niwax</span> Mobile App Development Company</h2>
-          <p>Lorem Ipsum is text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+  <section class="about-sec-app pad-tb pt-60 dark-bg2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="common-heading text-l">
+            <h4 class="mb-30"> At Evolve, we have curated a comprehensive lead generation ecosystem – built on
+              best-in-class practices. Within this ecosystem of industry experts, we provide unique dedicated communities
+              for each client.</h4>
+            
+            <h5>Having a dedicated unit just for your needs allows us to ensure -</h5>
 
-          <p class="mt-10">Lorem Ipsum is text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-          <p class="mt-10"> <span class="text-bold">Lorem Ipsumis simply dummy text of the printing and typesetting industry. Simply dummy text of the printing and typesetting industry. </span></p>
+            <p class="mt-10">Alignment with your culture and core values</p>
+            <p class="mt-10">Teams trained to be your extension units with complete visibility and control.</p>
+            <p class="mt-10">Predictable outcomes through shared planning and execution. </p>
+            <p class="mt-10">Transparency – Of how your unit is functioning through access
+              to operational metrics and systems. </p>
+
+            <p class="mt-10"> Accountability – Of our deliveries.</p>
+
+            <p class="mt-10">Authenticity – Of every lead we capture.</p>
+
+            <p class="mt-10">Security – Of your & clients and data. </p>
+
+
+          </div>
+          <!-- <div class="cta-card mt-40">
+            <a href="#" class="btn-outline lnk">Request A Quote<i class="fas fa-chevron-right fa-icon"></i><span
+                class="circle"></span></a>
+          </div> -->
         </div>
-        <div class="cta-card mt-40">
-          <a href="#" class="btn-outline lnk">Request A Quote<i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
-        </div>
-      </div>
-      <div class="col-lg-6">
-        <div class="funfact">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl1">
-                <img src="images/icons/startup.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">20</span><span class="services-cuntr">+</span>
-                <p>Years Experience</p>
+        <div class="col-lg-6">
+          <div class="funfact">
+            <div class="row">
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl1">
+                  <img src="images/icons/startup.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">25</span><span class="services-cuntr">+</span>
+                  <p>Global Clients</p>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl2">
-                <img src="images/icons/team.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">250</span><span class="services-cuntr">+</span>
-                <p>Talented Squad</p>
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl2">
+                  <img src="images/icons/team.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">1M</span><span class="services-cuntr">+</span>
+                  <p>Leads Generated</p>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl3">
-                <img src="images/icons/mobile.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">1200</span><span class="services-cuntr">+</span>
-                <p>Apps Developed</p>
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl2">
+                  <img src="images/icons/mobile.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">2%</span><span class="services-cuntr">+</span>
+                  <p>Rejects</p>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl4">
-                <img src="images/icons/computers.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">100</span><span class="services-cuntr">%</span>
-                <p>Projects Delivered</p>
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl2">
+                  <img src="images/icons/computers.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">100</span><span class="services-cuntr">%</span>
+                  <p>accuracy of lead delivered</p>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl5">
-                <img src="images/icons/world.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">120</span><span class="services-cuntr">+</span>
-                <p>Countries Served</p>
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl5">
+                  <img src="images/icons/world.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">95%</span><span class="services-cuntr">+</span>
+                  <p>Accuracy of lead mobile numbers delivered</p>
+                </div>
               </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 col-6">
-              <div class="funfct srcl1">
-                <img src="images/icons/deal.svg" alt="niwax app development template">
-                <span class="services-cuntr counter">100</span><span class="services-cuntr">%</span>
-                <p>Client Satisfaction</p>
+              <div class="col-lg-4 col-md-6 col-sm-12 col-6">
+                <div class="funfct srcl1">
+                  <img src="images/icons/deal.svg" alt="niwax app development template">
+                  <span class="services-cuntr counter">50%</span><span class="services-cuntr">%</span>
+                  <p>Reduction in sales cycle</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 </template>
     
 <script>
 
 export default {
-    name: 'FunFact',
+  name: 'FunFact',
 }
 </script>
--- components/Index/Statistics.vue
+++ components/Index/Statistics.vue
@@ -4,8 +4,6 @@
             <div class="row justify-content-center">
                 <div class="col-lg-8">
                     <div class="common-heading">
-                        <span>Our Work</span>
-                        <h2 class="mb-0">Our Latest Creative Work</h2>
                     </div>
                 </div>
             </div>
@@ -13,27 +11,37 @@
                 <div class="col-lg-12 col-sm-12 wptbb">
                     <div class="pbwide shadow bg-gradient1">
                         <div class="portfolio-item-info-tt">
-                            <div class="logowide mb-20"><img src="images/client/customer-logo-5.png" alt="logo"
+
+                            <!-- <div class="logowide mb-20"><img src="images/client/customer-logo-5.png" alt="logo"
                                     class="img-fluid"></div>
-                            <div class="widebloktag"><span>Design</span> <span>Coding</span> <span>Design</span> </div>
-                            <h3 class="mt-30 mb-30">Weather & Radar - Accurate Weather Forecast</h3>
-                            <ul class="info-list-ul">
+                            <div class="widebloktag"><span>Design</span> <span>Coding</span> <span>Design</span> </div> -->
+                            <h3 class="mt-30 mb-30">Statistics</h3>
+                            <!-- <ul class="info-list-ul">
                                 <li>Product Strategy</li>
                                 <li>Product UI/UX Design</li>
                                 <li>Branding Design</li>
                                 <li>Design System</li>
-                            </ul>
-                            <a href="#" class="btn-outline lnk mt-30">View Case Study <i
-                                    class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
+                            </ul> -->
+                            <p> We work exclusively with agencies and publishers as
+                                their white-labelled delivery partner.</p>
+
+                            <p> Focussed on building and delivering best in class
+                                operational capacity for agencies. </p>
+
+                            <p>Dedicated operational teams with Chinese walls
+                                between agencies teams. </p>
+
+                            <p> Shared resources for data / technology / HR</p>
+                            <p>Innovation led partnership with ability to white label
+                                technology frameworks. </p>
                         </div>
                         <div class="portfolio-wide-image">
                             <div class="img-wide-blocktt tilt-outer">
                                 <div class="innerwidedevice tilt-inner" data-tilt data-tilt-max="4" data-tilt-speed="1000"
                                     data-tilt-perspective="2000">
-                                    <div class="desktopblock shadow1"><img src="images/portfolio/portfolio-wide-1.jpg"
-                                            alt="img" class="img-fluid"> </div>
-                                    <div class="mobileblock shadow1"><img src="images/portfolio/portfolio-wide-1a.jpg"
-                                            alt="img" class="img-fluid"> </div>
+                                    <div class="desktopblock shadow1"><img
+                                            src="~/assets/images/portfolio/real-homeright.png" alt="img" class="img-fluid">
+                                    </div>
                                 </div>
                             </div>
                         </div>
