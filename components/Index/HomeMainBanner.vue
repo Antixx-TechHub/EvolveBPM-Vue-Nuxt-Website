d5c80df427d070ff3d4d12ac5b2b23bc4a40d6ab
AntixxTechHub <info@antixxtechhub.com>
2024-01-24 15:53:53 +0530

    Home Banner

--- components/Index/HomeMainBanner.vue
+++ components/Index/HomeMainBanner.vue
@@ -1,29 +1,28 @@
 <template>
-    <!-- Start Hero -->
-    <section class="hero-section lead-hero">
-      <div class="text-block">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 d-flex align-items-center">
-              <div class="header-heading">
-                <h1 class="wow fadeInUp" data-wow-delay=".2s">We Analyze & Understand Your Buyers</h1>
-                <p class="wow fadeInUp" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus, risus sit amet auctor sodales, justo erat tempor eros.</p>
-                <a href="#" class="btn-main bg-btn lnk wow fadeInUp" data-wow-delay=".6s">Get Strategy Call <i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></a>
-              </div>
+  <div class="hero-section lead-hero">
+    <div class="text-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 d-flex align-items-center">
+            <div class="header-heading">
+              <h3 class="wow fadeInUp" data-wow-delay=".2s">Empower your sales team with impactful revenue solutions</h3>
+              <p class="wow fadeInUp" data-wow-delay=".4s">Our state-of-the-art AI, Intent, and Tech Insights-driven
+                demand generation solutions are designed to supercharge your sales force.</p>
+              <a href="#" class="btn-main bg-btn4 lnk">Explore <i class="fas fa-chevron-right fa-icon"></i><span
+                  class="circle"></span></a>
             </div>
-            <!-- <div class="col-lg-6 wow fadeIn" data-wow-delay=".4s">
+          </div>
+          <!-- <div class="col-lg-6 wow fadeIn" data-wow-delay=".4s">
               <div class="single-image"><img src="images/hero/leads-hero.png" alt="hero image" class="img-fluid" /></div>
             </div> -->
-          </div>
         </div>
       </div>
-    </section>
-    <!-- End Hero -->
-  </template>
+    </div>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'HomeMainBanner',
-  };
-  </script>
-  
+<script>
+export default {
+  name: 'HomeMainBanner',
+};
+</script> 
