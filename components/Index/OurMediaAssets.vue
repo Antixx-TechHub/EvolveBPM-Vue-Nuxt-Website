e2ec874c5012f211bc83edc4c812087688ca435c
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-01-25 17:26:25 +0530

    media assets component done

--- components/Index/OurMediaAssets.vue
+++ components/Index/OurMediaAssets.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="testinomial-section-app bg-none pad-tb">
         <div class="container">
+            <h6 style="text-align: center"><span class="text-second text-bold">OUR HAPPY CUSTOMERS</span></h6>
+            <h2 style="text-align: center" class="mb-4"><span style="color:#6A6A8E">Our Media Assets</span></h2>
+
             <div class="row">
                 <div class="col-lg-6">
                     <div class="pl-50">
@@ -8,24 +11,30 @@
                             <!-- <img src="images/shape/shape-3.svg" alt="shape"> -->
                         </div>
                         <div class="testimonial-card-a tcd owl-carousel">
-                            <div class="testimonial-card">
-                                <div class="tt-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                        unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            <div class="testimonial-card ">
+                                <h4>Here is where your audiences thrive in the Evolve ecosystem.</h4>
+                                <div class="tt-text pt30">
+                                    <p>Do you wonder the impact of reaching your audience through sources they trust? With
+                                        the changing spam laws, the best way to reach your audience remains committees they
+                                        frequently visit. We actively function to create a hub of those!
                                     </p>
+                                </div>
+                                <div class="tt-text pt30">
+                                    <p>We further leverage this audience to curate disruptive lead generation strategies.
+                                        Each strategy is uniquely suited to your needs – adding our end users as highly
+                                        qualified leads to your dedicated community, one campaign at a time</p>
                                 </div>
                                 <div class="client-thumbs mt-30">
                                     <div class="media v-center upset">
                                         <div class="user-image bdr-radius">
-                                            <img src="images/user-thumb/girl.jpg" alt="girl"
-                                                class="img-fluid rounded-circle" />
-                                            </div>
-                                        <div class="media-body user-info v-center">
+                                            <!-- <img src="images/user-thumb/girl.jpg" alt="girl"
+                                                class="img-fluid rounded-circle" /> -->
+                                        </div>
+                                        <!-- <div class="media-body user-info v-center">
                                             <h5>Mike Smith</h5>
                                             <p>Founder at Itawa Real eState</p>
                                             <i class="fas fa-quote-right posiqut"></i>
-                                        </div>
+                                        </div> -->
                                     </div>
                                 </div>
                             </div>
@@ -36,43 +45,52 @@
                     <div class="clients-logos text-center col-12">
                         <ul class="row text-center clearfix">
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".2s">
-                                <div class="brand-logo"><img src="~/assets/images/logofour.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logofour.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".4s">
-                                <div class="brand-logo"><img src="~/assets/images/logofive.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logofive.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay="3.6s">
-                                <div class="brand-logo"><img src="~/assets/images/logosix.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logosix.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                         </ul>
                         <ul class="row text-center clearfix">
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".2s">
-                                <div class="brand-logo"><img src="~/assets/images/logosevn.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logosevn.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".4s">
-                                <div class="brand-logo"><img src="~/assets/images/logoeight.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logoeight.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay="3.6s">
-                                <div class="brand-logo"><img src="~/assets/images/logonine.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logonine.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                         </ul>
                         <ul class="row text-center clearfix">
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".2s">
-                                <div class="brand-logo"><img src="~/assets/images/logosevn.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logosevn.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay=".4s">
-                                <div class="brand-logo"><img src="~/assets/images/logoeight.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logoeight.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                             <li class="col-lg-4 col-md-3 col-sm-4 col-6 wow fadeIn" data-wow-delay="3.6s">
-                                <div class="brand-logo"><img src="~/assets/images/logonine.png" alt="clients" class="img-fluid">
+                                <div class="brand-logo"><img src="~/assets/images/logonine.png" alt="clients"
+                                        class="img-fluid">
                                 </div>
                             </li>
                         </ul>
