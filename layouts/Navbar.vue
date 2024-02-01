22271df0598bd62a8bd883620a796cb694d825a1
Srohanrs <prashant.bhosale@antixxtechhub.com>
2024-02-01 15:23:54 +0530

    let's start and data strategey menu trans done

--- layouts/Navbar.vue
+++ layouts/Navbar.vue
@@ -4,8 +4,8 @@
             <div class="container m-pad">
                 <nav class="menu-header">
                     <NuxtLink class="dsk-logo nav-brand" to="/">
-                        <!-- <img src="~/assets/images/logo.png" class="black-logo" alt="logo"> -->
-                        <img src="~/assets/images/white-logo.png" alt="logo">
+                        <img src="~/assets/images/logo.png" class="black-logo" alt="logo">
+                        <!-- <img src="~/assets/images/white-logo.png" alt="logo"> -->
                     </NuxtLink>
 
                     <div>
